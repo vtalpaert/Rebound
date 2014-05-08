4b55fab85b1b8629449e6c8f726b2e97aa0c6088
Braaaains <victor.talpaert@gmail.com>
2014-05-08 16:31:17 +0200

    import in robot.py now works

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ host = '0.0.0.0'  # '0.0.0.0' means listening on every IP address
 channel = 233
 
 # All the info will be written in the same log, even when info comes from other scripts
-logging.basicConfig(filename=log_path, format='%(asctime)s %(message)s', level=logging.INFO)
+logging.basicConfig(filename=log_path, format='%(asctime)s %(message)s', level=logging.DEBUG)
+logging.debug('======== STARTING SCRIPT ===========')
 
 class Server(SocketServer.TCPServer):
     def __init__(self, server_address, handlerClass):
@@ -22,35 +23,37 @@ class Server(SocketServer.TCPServer):
         try :
             SocketServer.TCPServer.__init__(self, server_address, handlerClass)
         except IOError as details:
-            logging.error('socket.error : {}'.format(details))
+            logging.error('Server : socket.error : {}'.format(details))
         
-        if robot.rpi :
-            self.robot = robot.Robot()
+        logging.info('Server : RPi library : {}'.format(robot.GPIO))
+        if robot.GPIO is None :
+            self.robo = robot.BaseRobot()
         else :
-            self.robotThread = robot.BaseRobot()
-        logging.info('Server : Initialization complete')
+            self.robo = robot.Robot()
 
 
 class Handler(SocketServer.BaseRequestHandler):
     # Instantiated once per connection        
     def handle(self):
-        logging.info('Socket : {} is connected'.format(self.client_address))
+        logging.info('Handler : {} is connected'.format(self.client_address))
         while True:
             self.order = self.request.recv(256).strip()
             if not self.order: break
-            logging.info('Socket : Order is ##%s##' % self.order)
-            self.server.robotThread.execute(self.order)
+            logging.info('Handler : Order is ##%s##' % self.order)
+            self.server.robo.execute(self.order)
         self.request.close()
-        logging.info('Socket : {} is disconnected'.format(self.client_address))
+        logging.info('Handler : {} is disconnected'.format(self.client_address))
 
 
 if __name__ == '__main__':
     print 'log can be found at ' + log_path
+    logging.debug('Main : Starting server')
     sServer = Server((host, channel), Handler)
-    logging.info('Server : Ready to serve on {}'.format(sServer.server_address))
+    logging.info('Main : Ready to serve on {}'.format(sServer.server_address))
     try :
         sServer.serve_forever()
     except KeyboardInterrupt as details :
         sServer.server_close()
-        logging.info(' closing server ({})'.format(details))
-    logging.info('Server : SocketServer stopped serving')
+        logging.info('Main : closing server ({})'.format(details))
+    finally:
+        logging.info('Main : SocketServer stopped serving')
--- robot.py
+++ robot.py
@@ -6,16 +6,32 @@ Created on 8 mai 2014
 
 # In this file are listed functions specific to the Raspberry Pi
 
-import logging
-print 'someone called me'
-try:
-    import RPi.GPIO as GPIO
-    rpi = True
-except ImportError as details :
-    logging.info(' no RPi library found ({})'.format(details))
-    rpi = False
-    
-print rpi
+import logging, sys
+
+# try:
+#     import RPi.GPIO as GPIO
+# except ImportError as details :
+#     logging.info('Robot : no RPi library found ({})'.format(details))
+#     rpi = False
+# else :
+#     rpi = True
+
+
+# solution found at https://lucumr.pocoo.org/2011/9/21/python-import-blackbox/
+def import_module(module_name):
+    try:
+        __import__(module_name)
+    except ImportError:
+        exc_type, exc_value, tb_root = sys.exc_info()
+        tb = tb_root
+        while tb is not None:
+            if tb.tb_frame.f_globals.get('__name__') == module_name:
+                raise exc_type, exc_value, tb_root
+            tb = tb.tb_next
+        return None
+    return sys.modules[module_name]
+
+GPIO = import_module('RPi.GPIO')
 
 
 class BaseRobot(object):
@@ -72,38 +88,38 @@ class BaseRobot(object):
 
     # when quitting, executed from server.py
     def quit(self):
-        logging.info('End of simulation')
+        logging.info('Robot : End of simulation')
 
 class Robot(BaseRobot): 
- 
+   
     def __init__ (self):
-         
+           
         # parameters
         self.n = 11  # GPIO number for the LED
         self.h = 15  # input 1
         self.j = 16  # input 2
         self.k = 18  # input 3
         self.l = 22  # input 4
- 
+   
         # GPIO initialization
         GPIO.cleanup()  # used to avoid bugs if restarting the server
         GPIO.setmode(GPIO.BOARD)
         for i in [self.n, self.h, self.j, self.k, self.l]:
             GPIO.setup(i, GPIO.OUT)
             GPIO.output(i, GPIO.LOW)
- 
+   
         BaseRobot.__init__(self)
- 
- 
+   
+   
     # Full definition of the 7 functions
     def zero(self):
         logging.info('Robot : LED goes to 0')
         GPIO.output(self.n, GPIO.LOW)
- 
+   
     def un(self):
         logging.info('Robot : LED goes to 1')
         GPIO.output(self.n, GPIO.HIGH)
- 
+   
     def arret(self):
         logging.info('Robot : Robot stops')
         GPIO.output(self.n, GPIO.LOW)
@@ -111,39 +127,37 @@ class Robot(BaseRobot):
         GPIO.output(self.k, GPIO.LOW)
         GPIO.output(self.j, GPIO.LOW)
         GPIO.output(self.l, GPIO.LOW)
- 
+   
     def avance(self):
         logging.info('Robot : Goes forward')
         GPIO.output(self.j, GPIO.LOW)
         GPIO.output(self.l, GPIO.LOW)
         GPIO.output(self.h, GPIO.HIGH)
         GPIO.output(self.k, GPIO.HIGH)
- 
+   
     def recule(self):
         logging.info('Robot : Goes backward')
         GPIO.output(self.h, GPIO.LOW)
         GPIO.output(self.k, GPIO.LOW)
         GPIO.output(self.j, GPIO.HIGH)
         GPIO.output(self.l, GPIO.HIGH)
- 
+   
     def droite(self):
         logging.info('Robot : Goes right')
         GPIO.output(self.j, GPIO.LOW)
         GPIO.output(self.k, GPIO.LOW)
         GPIO.output(self.h, GPIO.HIGH)
         GPIO.output(self.l, GPIO.HIGH)
- 
+   
     def gauche(self):
         logging.info('Robot : Goes left')
         GPIO.output(self.h, GPIO.LOW)
         GPIO.output(self.l, GPIO.LOW)
         GPIO.output(self.j, GPIO.HIGH)
         GPIO.output(self.k, GPIO.HIGH)
- 
+   
     # clean end of program
     def quit(self):
         GPIO.cleanup()
-        logging.info('Cleaned GPIOs')
-
-
+        logging.info('Robot : Cleaned GPIOs')
 
